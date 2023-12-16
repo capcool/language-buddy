93f34000574be046ebd2a45476b6da7908c3b02c
capcool <sudipta15ee@gmail.com>
2023-12-16 20:38:49 +0530

    first commit

---
+++ app/components/speak.js
@@ -0,0 +1,9 @@
+export default function Speak(){
+    return(
+        <>
+        <button>speak</button>
+        <button>stop</button>
+        <button>reset</button>
+        </>
+    )
+}
---
+++ app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+import styles from './page.module.css'
+import Speak from './components/speak'
+
+export default function Home() {
+  return (
+   <>
+   <div>Hello World</div>
+   <Speak/>
+   </>
+  )
+}
